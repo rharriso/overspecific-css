ea4359dbef2ce7e9ac76949335a7ebc6030fb0f0
Ross Harrison <rtharrison86@gmail.com>
2016-10-16 13:03:31 -0400

    running tests from fixtures

--- test/Lexer.test.js
+++ test/Lexer.test.js
@@ -1,21 +1,44 @@
 'use strict';
 
 import assert from 'assert';
+import { expect } from 'chai';
 import _ from 'lodash';
 
 import Lexer from '../src/Lexer'
+import fs from 'fs'
 
-describe('Lexer', () => { 
-  describe('#parse(cssText)', () => { 
-    it('should return an empty array with passed and empty string', () => {
-      const result = Lexer.parse('');
-      assert(_.isArray(result));
-      assert(_.isEmpty(result));
+
+function runCase (testCase) {
+  describe('Lexer', () => { 
+    describe('#parse(cssText)', () => { 
+      it(testCase.name, (done) => {
+        const fixtureFile = './test/fixtures/Lexer/'+testCase.file;
+
+        fs.readFile(fixtureFile, 'utf8', function (err,data) {
+          if (err) {
+            done(err);
+          }
+
+          var result = Lexer.parse(data.toString());
+          expect(result).to.deep.equal(testCase.result);
+          done();
+        });
+      });
     });
-    
-    it('should return one')
-    
-    it('should return multiple selectors')
   });
-});
+}
 
+const testCases = [
+  {
+    name: "Empty css file",
+    file: "empty.css",
+    result: []
+  },
+  { 
+    name: "Single Selctor",
+    file: "single-selector.css",
+    result: [['.class-one']]
+  }
+];
+
+testCases.map(runCase); 
